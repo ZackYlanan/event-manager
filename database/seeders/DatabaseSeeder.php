0f755ddd7fb51b1d6defcac2ba323b4a38100d29
ZackYlanan <isaacylanan@gmail.com>
2026-06-14 11:42:45 +0800

    add new columns in the user table, the new structure uses RBAC so we have student role.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\EventCategory;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +19,36 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'System Administrator',
+            'email' => 'admin@pup.edu',
+            'password' => Hash::make('admin123'),
+            'role' => 'admin',
+            'student_id' => null,
+            'course' => null,
+        ]);
+
+        // create for categories
+
+        EventCategory::insert([
+            [
+                'category' => 'hackathon',
+                'display_name' => 'University Hackathon',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'category' => 'seminar',
+                'display_name' => 'Academic Seminar',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'category' => 'workshop',
+                'display_name' => 'Skills Workshop',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
         ]);
     }
 }
