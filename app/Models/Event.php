cebf36858354694a6a774789f391e6a50917bf1b
ZackYlanan <isaacylanan@gmail.com>
2026-06-14 09:25:01 +0800

    add event, event category, registration and modified user model

---
+++ app/Models/Event.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'admin_id',
+        'category_id',
+        'title',
+        'description',
+        'venue',
+        'event_date',
+        'start_time',
+        'end_time',
+        'maximum_slots',
+        'registration_deadline',
+        'status',
+    ];
+
+    protected $cast = [ // $cast automatically converts database values to specific PHP data types when retrieving them
+        'event_date' => 'date',
+        'registration_deadline' => 'date',
+    ];
+
+    public function admin()
+    {
+        return $this->belongsTo(User::class, 'admin_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(EventCategory::class, 'category_id');
+    }
+
+    public function registrations()
+    {
+        return $this->hasMany(Registration::class, 'event_id');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,15 @@ class User extends Authenticatable
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+        'student_id',
+        'course'
+    ];
+
     /**
      * Get the attributes that should be cast.
      *
@@ -28,5 +37,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function createdEvents()
+    {
+        return $this->hasMany(Event::class, 'admin_id');
+    }
+
+    public function registrations()
+    {
+        return $this->hasMany(Registration::class, 'user_id');
     }
 }
